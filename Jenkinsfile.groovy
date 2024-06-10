ef23a9b75754bd2616b8c7826b11817f7afc31b6
Andrei Murillo <andrei_murillo@epam.com>
2024-06-10 11:00:29 -0600

    Jenkinsfile added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'BRANCH', choices: ['main', 'dev'], description: 'Select the branch to deploy')
+        string(name: 'IMAGE_TAG', defaultValue: 'v1.0', description: 'Docker image tag')
+    }
+    environment {
+        BRANCH_NAME = "${params.BRANCH}"
+        PORT = "${BRANCH_NAME == 'main' ? '3000' : '3001'}"
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: "*/${params.BRANCH}"]], userRemoteConfigs: [[url: '<tu_repositorio_url>']]])
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    def imageName = BRANCH_NAME == 'main' ? "nodemain:${params.IMAGE_TAG}" : "nodedev:${params.IMAGE_TAG}"
+                    sh "docker build -t ${imageName} ."
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                script {
+                    def imageName = BRANCH_NAME == 'main' ? "nodemain:${params.IMAGE_TAG}" : "nodedev:${params.IMAGE_TAG}"
+                    sh """
+                        docker stop ${BRANCH_NAME} || true
+                        docker rm ${BRANCH_NAME} || true
+                        docker run -d --expose ${PORT} -p ${PORT}:3000 --name ${BRANCH_NAME} ${imageName}
+                    """
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                sh "docker system prune -f"
+            }
+        }
+    }
+}
